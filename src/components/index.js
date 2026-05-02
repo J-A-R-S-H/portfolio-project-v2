b48b16ed48476c668b210e5097df875b1fed6c90
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2026-05-01 17:23:08 -0700

    finished making changes to window store

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
-import TerminalWindow from "../windows/Terminal.jsx";
+import TerminalWindow from "#window/Terminal.jsx";
 import WindowControls from "./WindowControls.jsx";
-export { Navbar, Welcome, Dock, TerminalWindow as Terminal, WindowControls };
+
+export {Navbar, Welcome, Dock, TerminalWindow as Terminal, WindowControls};
